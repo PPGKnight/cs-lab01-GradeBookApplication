856327106aae591d756c1849bd07b32a53cbf64c
PPGKnight <kuba.hankus@gmail.com>
2022-04-01 13:05:46 +0200

    Override Ranked

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,20 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            if (averageGrade > 80)
+                return 'A';
+            if (averageGrade > 60)
+                return 'B';
+            if (averageGrade > 40)
+                return 'C';
+            if (averageGrade > 20)
+                return 'D';
+            return 'F';
+
+        }
     }
 }
