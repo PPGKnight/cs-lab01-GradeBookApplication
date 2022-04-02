9b4f0e7ea12fed2d142c0706027992f416d89273
PPGKnight <kuba.hankus@gmail.com>
2022-04-02 19:18:02 +0200

    Added IsWeighted

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,11 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
         
         public GradeBookType Type { get; set; }
